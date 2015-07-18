60859fb989550c36b6b6506140d255262caae45e
shigemk2 <i.am.shige@gmail.com>
2015-07-18 17:28:52 +0900

    Fix

--- chap01/chap01a.hs
+++ chap01/chap01a.hs
@@ -8,4 +8,5 @@ notElem' :: Eq a => [a] -> a -> Bool
 notElem' a n = notElem n a
 
 main = do
+    print $ [1..9] \\ [3]
     print $ minfree [08, 23, 09, 00, 12, 11, 01, 10, 13, 07, 41, 04, 14, 21, 05, 17, 03, 19, 02, 06]
