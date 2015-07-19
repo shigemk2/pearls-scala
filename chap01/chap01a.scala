9e0e9709645621fc608d6031cf75c2ee40d8cd0a
shigemk2 <i.am.shige@gmail.com>
2015-07-19 16:05:58 +0900

    delete comments

--- chap01/chap01a.scala
+++ chap01/chap01a.scala
@@ -1,16 +1,3 @@
-// minfree :: [Integer] -> Integer
-// minfree xs = head([0..] \\ xs)
-
-// (\\) :: Eq a => [a] -> [a] -> [a]
-// us \\ vs = filter (notElem' vs) us
-
-// notElem' :: Eq a => [a] -> a -> Bool
-// notElem' a n = notElem n a
-
-// main = do
-//     print $ [1..9] \\ [3]
-//     print $ minfree [08, 23, 09, 00, 12, 11, 01, 10, 13, 07, 41, 04, 14, 21, 05, 17, 03, 19, 02, 06]
-
 def minfree(xs: List[Int]): Int = {
   // minfree xs = head([0..] \\ x)
   // Scalaでこういう書き方出来ない
